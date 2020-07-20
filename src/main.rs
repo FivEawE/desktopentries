870b814a624cc5377b1202c3404157cef9b70b3d
FivEawE <fiveawe@gmail.com>
2020-07-20 11:23:07 +0200

    Couple of fixes

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,12 @@ use crate::checker::Checker;
 use crate::configuration::Configuration;
 use crate::entry::Entry;
 use std::collections::HashMap;
-use std::env;
 use std::fs;
 use std::path::{Path, PathBuf};
+use std::{
+    env,
+    io::{self, BufWriter, Write},
+};
 use structopt::StructOpt;
 
 mod checker;
@@ -20,28 +23,41 @@ fn main() {
     match xdg_data_dirs {
         Ok(value) => {
             for dir in value.split(':') {
-                get_entries_from_path(dir, Path::new(dir), &mut entries);
+                let mut path = PathBuf::from(dir);
+                path.push("applications/");
+
+                get_entries_from_path(&path.display().to_string(), path.as_path(), &mut entries);
             }
         }
         Err(_) => {
-            let base_path = "/usr/share/applications";
+            let base_path = "/usr/share/applications/";
             eprintln!("$XDG_DATA_DIRS not set, defaulting to {}", base_path);
 
-            get_entries_from_path(base_path, Path::new(base_path), &mut entries);
+            get_entries_from_path(&String::from(base_path), Path::new(base_path), &mut entries);
         }
     }
 
-    for entry_tuple in entries {
-        let contents = fs::read_to_string(&entry_tuple.1);
+    let stdout = io::stdout();
+    let mut out_handle = BufWriter::new(stdout);
+
+    for entry_path in entries.values() {
+        let contents = fs::read_to_string(entry_path);
         match contents {
             Ok(contents) => {
                 let entry = Entry::new(&contents);
                 match entry {
                     Ok(entry) => {
                         if checker.check_entry(&entry) {
-                            println!("{}", entry_tuple.1.display());
-                            println!("{}", contents);
-                            println!();
+                            let out = writeln!(out_handle, "{}", entry_path.display());
+                            match out {
+                                Ok(_) => {
+                                    let out = writeln!(out_handle, "{}", contents);
+                                    if let Err(error) = out {
+                                        eprintln!("{}", error);
+                                    }
+                                }
+                                Err(error) => eprintln!("{}", error),
+                            }
                         }
                     }
                     Err(error) => eprintln!("{}", error),
@@ -52,7 +68,7 @@ fn main() {
     }
 }
 
-fn get_entries_from_path(base_path: &str, path: &Path, entries: &mut HashMap<String, PathBuf>) {
+fn get_entries_from_path(base_path: &String, path: &Path, entries: &mut HashMap<String, PathBuf>) {
     let dir_iterator = fs::read_dir(path);
     match dir_iterator {
         Ok(dir_iterator) => {
@@ -71,8 +87,8 @@ fn get_entries_from_path(base_path: &str, path: &Path, entries: &mut HashMap<Str
                                 } else if file_type.is_file() {
                                     let file_name = file.path().display().to_string();
                                     if file_name.ends_with(".desktop") {
-                                        let key = String::from(&file_name[base_path.len()..])
-                                            .replace('/', "-");
+                                        let key =
+                                            file_name.replace(base_path, "").replace('/', "-");
                                         entries.entry(key).or_insert(file.path());
                                     }
                                 }
